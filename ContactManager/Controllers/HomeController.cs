691e181412168814dbe0c7db3f9d284f413a7087
Elihu Cruz <ea_2305@hotmail.com>
2016-04-14 19:38:30 -0500

    Update to conect in db

---
+++ ContactManager/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var model = new List<Tweets>();
+            var client = new HttpClient();
+            //Respond massages
+            var task =
+                client.GetAsync("http://search.twitter.com/search.json?q=pluralsight")
+                .ContinueWith((taskwithmsg) => {
+                    var response = taskwithmsg.Result;
+                    response.Content.ReadAsStringAsync<Json>();
+
+                }
+                );
+            task.Wait();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ContactManager/Scripts/CallAPI.js
@@ -0,0 +1,20 @@
+﻿console.log('datos cargados')
+
+
+console.log("information reddit");
+
+console.log('hola mundo')
+$.ajax({
+    //url: "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tag=animals",
+    url: "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0d28d131bc425440370cad6976a7d7dc}&tags=flower&per_page=3",
+    dataType: "jsonp",
+    jsonpCallback: 'jsonFlickrFeed',
+    cache: false,
+    succes: function (e) {
+        console.log(e);
+    },
+    error: function (e) {
+
+        console.log(e);
+    }   
+});
